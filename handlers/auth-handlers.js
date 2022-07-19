38f0b99a835920250100f22cb943c5d8491d0d4e
Evgenia-bit <ev.mo4alina@yandex.ru>
2022-07-19 16:49:52 +0300

    added status error codes

--- handlers/auth-handlers.js
+++ handlers/auth-handlers.js
@@ -9,23 +9,31 @@ module.exports = {
                 msg: 'Это страница авторизации. Отправьте запрос на authentication, чтобы войти в систему'
             })
         } catch (err) {
-            res.json({status: 'error', msg: 'Произошла ошибка: ' + err.message})
+            res.status(500).json({status: 'error', msg: 'Произошла ошибка: ' + err.message})
         }
     },
     authentication: async (req, res) => {
         try {
             const {nickname, password} = req.body
-            if (!nickname || !password) return res.json({status: 'error', msg: 'Вы не ввели данные'})
+            if (!nickname || !password) {
+                return res.status(400).json({status: 'error', msg: 'Вы не ввели данные'})
+            }
+
             const person = await db.getPerson(nickname)
+            if (!person) {
+                return res.status(400).json({status: 'error', msg: 'Вы ввели неверный ник'})
+            }
 
             const comparePassword = bcrypt.compareSync(password, person.password)
-            if (comparePassword) {
-                req.session.person_id = person.id
-                return res.json({status: 'OK', msg: 'Все прошло успешно!'})
+            if (!comparePassword) {
+                return res.status(400).json({status: 'error', msg: 'Вы ввели неверный пароль'})
             }
-            return res.json({status: 'error', msg: 'Вы ввели неверный пароль'})
+
+            req.session.person_id = person.id
+            return res.json({status: 'OK', msg: 'Все прошло успешно!'})
+
         } catch (err) {
-            res.json({status: 'error', msg: 'Произошла ошибка: ' + err.message})
+            res.status(500).json({status: 'error', msg: 'Произошла ошибка: ' + err.message})
         }
     },
     logout: async (req, res) => {
@@ -33,7 +41,7 @@ module.exports = {
             req.session.person_id = null
             return res.json({status: 'OK', msg: 'Вы успешно вышли из системы!'})
         } catch (err) {
-            res.json({status: 'error', msg: 'Произошла ошибка: ' + err.message})
+            res.status(500).json({status: 'error', msg: 'Произошла ошибка: ' + err.message})
         }
     }
 }
--- handlers/disks-handlers.js
+++ handlers/disks-handlers.js
@@ -6,7 +6,7 @@ module.exports = {
             const disks = await db.getOwnDisks(req.session.person_id)
             res.json({status: 'OK', title: 'Мои диски', disks})
         } catch (err) {
-            res.json({status: 'error', msg:  'Произошла ошибка: ' + err.message})
+            res.status(500).json({status: 'error', msg: 'Произошла ошибка: ' + err.message})
         }
     },
     getListFreeDisks: async (req, res) => {
@@ -14,7 +14,7 @@ module.exports = {
             const disks = await db.getFreeDisks(req.session.person_id)
             res.json({status: 'OK', title: 'Свободные диски', disks})
         } catch (err) {
-            res.json({status: 'error', msg:  'Произошла ошибка: ' + err.message})
+            res.status(500).json({status: 'error', msg: 'Произошла ошибка: ' + err.message})
         }
     },
     getListDisksTakenByPerson: async (req, res) => {
@@ -22,7 +22,7 @@ module.exports = {
             const disks = await db.getDisksTakenByPerson(req.session.person_id)
             res.json({status: 'OK', title: 'Диски, взятые мной', disks})
         } catch (err) {
-            res.json({status: 'error', msg:  'Произошла ошибка: ' + err.message})
+            res.status(500).json({status: 'error', msg: 'Произошла ошибка: ' + err.message})
         }
     },
     getListDisksTakenFromPerson: async (req, res) => {
@@ -30,36 +30,54 @@ module.exports = {
             const disks = await db.getDisksTakenFromPerson(req.session.person_id)
             res.json({status: 'OK', title: 'Диски, взятые у меня', disks})
         } catch (err) {
-            res.json({status: 'error', msg:  'Произошла ошибка: ' + err.message})
+            res.status(500).json({status: 'error', msg: 'Произошла ошибка: ' + err.message})
         }
     },
     giveAwayDisk: async (req, res) => {
         try {
             const diskId = req.query.id
-            if(!diskId) return res.json({status: 'error', msg: 'Данные не верны'})
+            if(!diskId) {
+                return res.status(400).json({status: 'error', msg: 'Вы не ввели ID диска'})
+            }
             const takenItem = await db.getTakenItem(diskId)
-            if(!takenItem) return res.json({status: 'error', msg: 'Такого диска не существует'})
-            if(takenItem.ownerid == req.session.person_id) return res.json({status: 'error', msg: 'Это ваш собственный диск!'})
-            if(takenItem.holderid != req.session.person_id) return res.json({status: 'error', msg: 'Вы не брали этот диск!'})
+            if(!takenItem) {
+                return res.status(404).json({status: 'error', msg: 'Такого диска не существует'})
+            }
+            if(takenItem.ownerid == req.session.person_id) {
+                return res.status(409).json({status: 'error', msg: 'Это ваш собственный диск!'})
+            }
+            if(takenItem.holderid != req.session.person_id) {
+                return res.status(409).json({status: 'error', msg: 'Вы не брали этот диск!'})
+            }
             await db.giveDisk(diskId)
             return res.json({status: 'OK', msg: 'Диск был успешно возвращён!'})
         } catch (err) {
-            res.json({status: 'error', msg:  'Произошла ошибка: ' + err.message})
+            res.status(500).json({status: 'error', msg: 'Произошла ошибка: ' + err.message})
         }
     },
     takeDisk: async (req, res) => {
         try {
             const diskId = req.query.id
-            if(!diskId ) return res.json({status: 'error', msg: 'Данные не верны'})
+            if(!diskId) {
+                return res.status(400).json({status: 'error', msg: 'Вы не ввели ID диска'})
+            }
             const takenItem = await db.getTakenItem(diskId)
-            if(!takenItem) return res.json({status: 'error', msg: 'Такого диска не существует'})
-            if(takenItem.ownerid == req.session.person_id) return res.json({status: 'error', msg: 'Это ваш собственный диск!'})
-            if(takenItem.holderid == req.session.person_id) return res.json({status: 'error', msg: 'Вы уже взяли этот диск!'})
-            if(takenItem.ownerid != takenItem.holderid) return res.json({status: 'error', msg: 'Этот диск уже взят другим пользователем!'})
+            if(!takenItem) {
+                return res.status(404).json({status: 'error', msg: 'Такого диска не существует'})
+            }
+            if(takenItem.ownerid == req.session.person_id) {
+                return res.status(409).json({status: 'error', msg: 'Это ваш собственный диск!'})
+            }
+            if(takenItem.holderid == req.session.person_id) {
+                return res.status(409).json({status: 'error', msg: 'Вы уже взяли этот диск!'})
+            }
+            if(takenItem.ownerid != takenItem.holderid) {
+                return res.status(409).json({status: 'error', msg: 'Этот диск уже взят другим пользователем!'})
+            }
             await db.takeDisk(req.session.person_id, diskId)
             res.json({status: 'OK', msg: 'Диск был успешно взят!'})
         } catch (err) {
-            res.json({status: 'error', msg:  'Произошла ошибка: ' + err.message})
+            res.status(500).json({status: 'error', msg: 'Произошла ошибка: ' + err.message})
         }
     }
 }
